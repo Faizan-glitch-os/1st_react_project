15f6e333436367e1452d9eacea5c9680f469a79b
Faizan Ahmad <faizanahmad2582001@gmail.com>
2024-06-19 15:57:56 +0500

    dynamic words can be added, image added using import

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
+import coreConceptImg from "./assets/react-core-concepts.png";
+
+const randomWords = ["Kaka", "Nana", "Wawa"];
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * randomWords.length);
+}
+
+function Header() {
+  const receivedRandomNumber = getRandomNumber();
+  console.log(receivedRandomNumber);
+
+  return (
+    <header>
+      <img src={coreConceptImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {randomWords[receivedRandomNumber]} React concepts you will need for
+        almost any app you are going to build!
+      </p>
+    </header>
+  );
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
         <h2>Time to get started!</h2>
       </main>
